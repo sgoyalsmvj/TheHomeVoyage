f3a184ba0d8ef560bfedaba11bbdf6c70e8de0a2
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-01-02 22:15:18 +0530

    image fix.

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,6 +1,6 @@
 export default function Image({ src, ...rest }) {
   // Assuming you have a variable for the base URL in your environment configuration
-  const baseUrl = process.env.REACT_APP_IMAGE_BASE_URL || '';
+  const baseUrl = import.meta.env.VITE_BASEURL || '';
 
   // Combine the base URL with the provided src
   const completeSrc = src && src.includes('https://') ? src : baseUrl + '/uploads/' + src;
