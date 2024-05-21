14a9f9e95272e33848fadb1ecca9ab6faf8db4d9
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-21 18:43:54 +0530

    fix : routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -10,16 +10,16 @@ const { googleAuthCallback } = require("../middlewares/auth.middleware");
 const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.get("/logout", logout);
+authRouter.post("register", register);
+authRouter.post("login", login);
+authRouter.get("logout", logout);
 
 // Google OAuth Routes
 authRouter.get(
-  "/google",
+  "google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-authRouter.get("/google/callback", googleAuthCallback, googleAuth);
+authRouter.get("google/callback", googleAuthCallback, googleAuth);
 
 module.exports = authRouter;
