5917b429f4d316575308c89135a762c241af0057
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-22 12:16:13 +0530

    fix : rename User.js

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String, // This makes the index ignore documents where googleId is null
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  mobileNum: {
+    type: String,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
