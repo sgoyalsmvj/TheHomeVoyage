8f6a2149ae6b2051f830bf868ad0741193d46c5d
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-05-22 12:02:21 +0530

    fix : remove unused imports

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const UserModel = require("./models/User");
 const dotenv = require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
